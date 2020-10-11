304162a52ac94ec75b943397687223763da8fcc6
tabishadnan <tabishadnan000@gmail.com>
2020-10-12 00:58:03 +0500

    react conditional routing without router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+state = {
+  page : "Home"
+}
+
+page = (newPage) => {
+  this.setState({
+    page : newPage
+  })
+}
+
+renderPage = () => {
+  if(this.state.page === "Home") return <Home />;
+  else if(this.state.page === "About") return <About />;
+  else if(this.state.page === "Contact") return <Contact />;
+}
+
+  render() {
+    return (
+      <div className="App">  
+        <ul>
+          <li><a onClick={() => this.page("Home")}>Home</a></li>
+          <li><a onClick={() => this.page("About")}>About</a></li>
+          <li><a onClick={() => this.page("Contact")}>Contact</a></li>
+        </ul>
+        {this.renderPage()}
+      </div>
+    );
+  }
 }
 
 export default App;
